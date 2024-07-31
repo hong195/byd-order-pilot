de145fdfa8e8e5e24baa762b5ad14bb1aecd27ea
aleksejem <alexeyhong10@gmail.com>
2024-07-31 14:55:23 +0500

    Fixed missing quality property.

--- src/Orders/Domain/Factory/RollFactory.php
+++ src/Orders/Domain/Factory/RollFactory.php
@@ -2,20 +2,31 @@
 
 namespace App\Orders\Domain\Factory;
 
-use App\Orders\Domain\Aggregate\Quality;
 use App\Orders\Domain\Aggregate\Roll\Roll;
 use App\Orders\Domain\Aggregate\Roll\RollType;
+use App\Orders\Domain\Aggregate\ValueObject\Quality;
 
+/**
+ * Class RollFactory.
+ *
+ * This class represents a factory for creating Roll objects.
+ */
 class RollFactory
 {
-    public function create(
-        string $name,
-        string $quality,
-        string $rollType,
-        int $priority = 0,
-        int $length = 0,
-        ?string $qualityNotes = null,
-    ): Roll {
+    /**
+     * Create a new Roll object.
+     *
+     * @param string      $name         the name of the roll
+     * @param string      $quality      the quality of the roll
+     * @param string      $rollType     the type of the roll
+     * @param int         $priority     the priority of the roll (optional)
+     * @param int         $length       the length of the roll (optional)
+     * @param string|null $qualityNotes any additional notes on the quality (optional)
+     *
+     * @return Roll the created Roll object
+     */
+    public function create(string $name, string $quality, string $rollType, int $priority = 0, int $length = 0, ?string $qualityNotes = null): Roll
+    {
         return new Roll(
             name: $name,
             quality: Quality::from($quality),
