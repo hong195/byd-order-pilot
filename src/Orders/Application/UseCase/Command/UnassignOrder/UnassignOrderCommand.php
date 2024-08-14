bd2763a4354f0435dd3f269cf9141b62ccdda4df
aleksejem <alexeyhong10@gmail.com>
2024-08-14 12:16:58 +0500

    Clean up commit.

---
+++ src/Orders/Application/UseCase/Command/UnassignOrder/UnassignOrderCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Orders\Application\UseCase\Command\ChangeOrderPriority;
+
+use App\Shared\Application\Command\CommandInterface;
+
+/**
+ * Represents a command to change the priority of an order.
+ */
+readonly class UnassignOrderCommand implements CommandInterface
+{
+    /**
+     * Constructor.
+     *
+     * @param int  $id       the ID of the object
+     * @param bool $priority the priority status of the object
+     */
+    public function __construct(public int $id, public bool $priority)
+    {
+    }
+}
