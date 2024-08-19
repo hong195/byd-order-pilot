e69c47852be9b0ea983a22fa7e73e5453a406dca
aleksejem <alexeyhong10@gmail.com>
2024-08-19 21:05:54 +0500

    Updated migrations.

--- src/Shared/Infrastructure/Database/Migrations/Version20240819160525.php
+++ src/Shared/Infrastructure/Database/Migrations/Version20240819160525.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240819154254 extends AbstractMigration
+final class Version20240819160525 extends AbstractMigration
 {
     public function getDescription(): string
     {
