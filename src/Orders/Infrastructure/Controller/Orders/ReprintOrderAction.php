a87f001f334a1e003562b2592e41f7587b3fbfd4
aleksejem <alexeyhong10@gmail.com>
2024-08-23 16:08:35 +0500

    Rename route.

--- src/Orders/Infrastructure/Controller/Orders/ReprintOrderAction.php
+++ src/Orders/Infrastructure/Controller/Orders/ReprintOrderAction.php
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
  * This class handles the find_order API endpoint.
  */
 #[AsController]
-#[Route('/api/rolls/{rollId}/reprint/orders/{orderId}', name: 'change-sort-order', requirements: ['rollId' => '^\d+$', 'orderId' => '^\d+$'], methods: ['POST'])]
+#[Route('/api/rolls/{rollId}/reprint/orders/{orderId}', name: 'reprint-order', requirements: ['rollId' => '^\d+$', 'orderId' => '^\d+$'], methods: ['POST'])]
 final readonly class ReprintOrderAction
 {
     /**
