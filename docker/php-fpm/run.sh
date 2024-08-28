e05e4c08f824408312e0db8cd6071f204837cbd2
aleksejem <alexeyhong10@gmail.com>
2024-08-28 19:19:26 +0500

    Added docker container right changing after its build.

--- docker/php-fpm/run.sh
+++ docker/php-fpm/run.sh
@@ -4,5 +4,7 @@
 procs=$(cat /proc/cpuinfo | grep processor | wc -l)
 sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 
+chown -R www-data:www-data /var/lib/nginx /var/log/nginx
+
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
