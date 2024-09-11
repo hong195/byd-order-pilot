d3e1998cb6fd1d12c051618b5b2f6f6d864438b2
aleksejem <alexeyhong10@gmail.com>
2024-09-11 19:20:15 +0500

    Roll history small refactor.

---
+++ src/Orders/Application/Service/Roll/History/HistoryListService.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Orders\Application\Service\Roll\History;
+
+use App\Orders\Application\DTO\EmployeeData;
+use App\Orders\Application\DTO\HistoryData;
+use App\Orders\Application\Service\Employee\EmployeeFetcher;
+use App\Orders\Domain\Repository\HistoryRepositoryInterface;
+
+/**
+ * Class HistoryListService.
+ */
+final readonly class HistoryListService
+{
+    /**
+     * Class MyClass.
+     */
+    public function __construct(private HistoryRepositoryInterface $historyRepository, private HistoryData $historyData, private EmployeeFetcher $employeeFetcher)
+    {
+    }
+
+    /**
+     * Retrieves the history data for a given roll ID.
+     *
+     * @param int $rollId the ID of the roll to retrieve history for
+     *
+     * @return HistoryData[] an array of HistoryData objects
+     */
+    public function __invoke(int $rollId): array
+    {
+        $histories = $this->historyRepository->findByRollId($rollId);
+        $employees = $this->employeeFetcher->getByIds(array_map(fn ($history) => $history->getEmployeeId(), $histories));
+
+        $historyData = [];
+        foreach ($histories as $history) {
+            $data = $this->historyData->fromHistory($history);
+            $employee = $employees->filter(fn (EmployeeData $employee) => $employee->id === $history->getEmployeeId())->first();
+
+            $data->withEmployee($employee ?: null);
+
+            $historyData[] = $data;
+        }
+
+        return $historyData;
+    }
+}
--- src/Orders/Application/UseCase/Query/FindRollHistory/FindRollHistoryHandler.php
+++ src/Orders/Application/UseCase/Query/FindRollHistory/FindRollHistoryHandler.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Orders\Application\UseCase\Query\FindRollHistory;
 
-use App\Orders\Application\DTO\EmployeeData;
-use App\Orders\Application\DTO\HistoryData;
 use App\Orders\Application\DTO\RollDataTransformer;
-use App\Orders\Application\Service\Employee\EmployeeFetcher;
-use App\Orders\Domain\Aggregate\Roll\History;
-use App\Orders\Domain\Repository\HistoryRepositoryInterface;
+use App\Orders\Application\Service\Roll\History\HistoryListService;
 use App\Orders\Domain\Repository\RollRepositoryInterface;
 use App\Shared\Application\AccessControll\AccessControlService;
 use App\Shared\Application\Query\QueryHandlerInterface;
@@ -26,11 +22,9 @@ final readonly class FindRollHistoryHandler implements QueryHandlerInterface
     /**
      * Class constructor.
      *
-     * @param AccessControlService       $accessControlService the access control service instance
-     * @param HistoryRepositoryInterface $historyRepository    the history repository instance
-     * @param HistoryData                $historyData          the history data instance
+     * @param AccessControlService $accessControlService the access control service instance
      */
-    public function __construct(private AccessControlService $accessControlService, private HistoryRepositoryInterface $historyRepository, private HistoryData $historyData, private EmployeeFetcher $employeeFetcher)
+    public function __construct(private AccessControlService $accessControlService, private HistoryListService $historyListService)
     {
     }
 
@@ -45,19 +39,6 @@ final readonly class FindRollHistoryHandler implements QueryHandlerInterface
     {
         AssertService::true($this->accessControlService->isGranted(), 'Access denied');
 
-        $histories = $this->historyRepository->findByRollId($rollQuery->rollId);
-        $employees = $this->employeeFetcher->getByIds(array_map(fn ($history) => $history->getEmployeeId(), $histories));
-
-        $historyData = [];
-        foreach ($histories as $history) {
-            $data = $this->historyData->fromHistory($history);
-            $employee = $employees->filter(fn (EmployeeData $employee) => $employee->id === $history->getEmployeeId())->first();
-
-            $data->withEmployee($employee ?: null);
-
-            $historyData[] = $data;
-        }
-
-        return new FindRollHistoryResult($historyData);
+        return new FindRollHistoryResult(($this->historyListService)($rollQuery->rollId));
     }
 }
