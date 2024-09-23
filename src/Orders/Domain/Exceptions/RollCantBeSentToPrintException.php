595feb54fbb11d78948ccdb26027427a950e4358
aleksejem <alexeyhong10@gmail.com>
2024-09-23 17:21:29 +0500

    Clean up commit.

--- src/Orders/Domain/Exceptions/RollCantBeSentToPrintException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Orders\Domain\Exceptions;
-
-final class RollCantBeSentToPrintException extends \Exception
-{
-}
--- src/ProductionProcess/Application/DTO/Product/ProductData.php
+++ src/ProductionProcess/Application/DTO/Product/ProductData.php
@@ -12,13 +12,12 @@ final readonly class ProductData
     /**
      * Class Constructor.
      *
-     * @param int                $id             the ID of the object
-     * @param bool               $hasPriority    whether the object has priority or not
-     * @param float              $length         the length of the object
-     * @param string             $filmType       the type of the film for the object
-     * @param \DateTimeInterface $addedAt        the date and time when the object was added
-     * @param string|null        $laminationType the type of lamination for the object (optional)
-     * @param string|null        $orderNumber    the order number for the object (optional)
+     * @param int         $id             the ID of the object
+     * @param bool        $hasPriority    whether the object has priority or not
+     * @param float       $length         the length of the object
+     * @param string      $filmType       the type of the film for the object
+     * @param string|null $laminationType the type of lamination for the object (optional)
+     * @param string|null $orderNumber    the order number for the object (optional)
      */
     public function __construct(
         public int $id,
@@ -26,7 +25,6 @@ final readonly class ProductData
         public float $length,
         public string $filmType,
         public ?string $orderNumber,
-        public \DateTimeInterface $addedAt,
         public ?string $laminationType = null,
     ) {
     }
